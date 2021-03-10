f293abed120f9fb5b1211e0885ec4ba181e11da9
Andrew Walker <andrew.walker.mn@gmail.com>
2021-03-10 00:27:44 -0600

    Lock test in progress

---
+++ testLock.cpp
@@ -0,0 +1,11 @@
+#include "uthread.h"
+#include "Lock.h"
+
+using namespace std;
+
+// lockQueue.push(new TCB(0, ORANGE, NULL, NULL, READY));
+
+int main(int argc, char *argv[]) {
+  Lock lock;
+  return 0;
+}
