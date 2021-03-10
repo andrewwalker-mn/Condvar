098e18de84e9eef525983fd3b27d384c10133432
David Ma <maxxx818@umn.edu>
2021-03-10 14:21:28 -0600

    condvar changes

--- CondVar.cpp
+++ CondVar.cpp
@@ -7,26 +7,32 @@ CondVar::CondVar() {
 
 void CondVar::wait(Lock &lock) {
   setLock(lock);
-  printf("%d", running->getId());
   disableInterrupts();
   lock._unlock();
   _waiting.push(running);
+  // running->setState(BLOCK);
+  // enableInterrupts();
+  switchThreads();
+  // switchThreads();
   enableInterrupts();
-  printf("%d", running->getId());
-  uthread_suspend(running->getId());
+  // uthread_yield();
   // enableInterrupts();
   lock.lock();
 }
 
 void CondVar::signal() {
   if(!_waiting.empty()) {
-    // disableInterrupts();
+    disableInterrupts();
     TCB *next = _waiting.front();
     _waiting.pop();
     _lock._unlock();
     _lock._signal(running);
+    // switchToThread(next); // we move this to _signal?
     // enableInterrupts();
-    switchToThread(next); // we move this to _signal?
+    // uthread_resume(next->getId());
+    switchToThread(next);
+    enableInterrupts();
+
     _lock.lock();
   }
 }
